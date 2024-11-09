bbd7c240a784263b6b5b1e7e1b2b4c364218ecb1
unknown <skshihab170@gmail.com>
2024-11-09 18:40:12 +0600

    php-day-2

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Hellow World"
+echo "Hellow World .My name is shihab";
+echo "7. PHP can generate dynamic page content 
+1. PHP can create, open, read, write, delete, and close files on the server
+2. PHP can collect form data
+3. PHP can send and receive cookies
+4. PHP can add, delete, modify data in your database
+5. PHP can be used to control user-access
+6. PHP can encrypt data"
 ?>
